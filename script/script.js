64a7d4648fb25a6787a02712794d5e881d32d41a
Berni <ndadesirarnold@icloud.com>
2025-10-06 12:56:49 +0000

    Ajouter un script pour verifier si une chaine est un palimdrome

---
+++ script/script.js
@@ -0,0 +1,25 @@
+const input = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn"); 
+const result = document.getElementById("result");
+
+checkButton.addEventListener("click", () => {
+    const inputText = input.value.trim(); 
+
+    if (inputText === "" ) {
+        alert("Please input a value");
+    }
+
+   if (isPalimdrome(inputText)) {
+        result.innerText = `${inputText} is a palindrome`; 
+   } else {
+       result.innerText = `${inputText} is not a palindrome`; 
+   }
+})
+
+//check if the string is a palindrome
+const isPalimdrome = (str) => {
+    const valideStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase(); 
+    const checked = [...valideStr].reverse().join(""); 
+
+   return valideStr === checked
+}
